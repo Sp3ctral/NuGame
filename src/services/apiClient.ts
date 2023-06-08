086ab16ec8517e37ef15f13efa1f697fd605e89a
Marvin Hozi <marvinhozi@yahoo.com>
2023-06-08 01:03:28 -0700

    Completed endpoint error handling and error displaying to user.

---
+++ src/services/apiClient.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export default axios.create(
+    {
+        baseURL: "https://api.rawg.io/api",
+        params: {
+            key: "baf13d58e83940618bf78c5640f91f44"
+        }
+    }
+);

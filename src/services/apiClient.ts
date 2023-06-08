0ccc6dc638ace0141d5437ddae07b79527fe38da
Marvin Hozi <marvinhozi@yahoo.com>
2023-06-08 01:44:16 -0700

    Comments on API key

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -7,6 +7,8 @@ export default axios.create(
     {
         baseURL: "https://api.rawg.io/api",
         params: {
+            // This key is public and is only used for testing.
+            // An API will replace this in production to protect the real key.
             key: "baf13d58e83940618bf78c5640f91f44"
         }
     }

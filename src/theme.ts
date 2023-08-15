4499f203611596886df736f4da6e7cc44947e2bd
Marvin Hozi <marvinhozi@yahoo.com>
2023-08-15 05:01:55 -0700

    Tuned dark mode theme colors

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,4 @@
-import {extendTheme, type ThemeConfig} from "@chakra-ui/react";
+import {Colors, extendTheme, type ThemeConfig} from "@chakra-ui/react";
 
 // The theme configuration for Chakra UI's provider.
 const config: ThemeConfig =
@@ -8,5 +8,21 @@ const config: ThemeConfig =
     useSystemColorMode: false
 }
 
-const theme = extendTheme({config});
+const colors: Colors =
+{
+    gray: {
+        50: "#f9f9f9",
+        100: "#ededed",
+        200: "#d3d3d3",
+        300: "#b3b3b3",
+        400: "#a0a0a0",
+        500: "#898989",
+        600: "#636363",
+        700: "#202020",
+        800: "#121212",
+        900: "#111111"
+    }
+}
+
+const theme = extendTheme({config, colors});
 export default theme;
